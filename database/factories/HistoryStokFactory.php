fd32d30a80f17da1771c08d2fd4ce92154d8bcf5
evftrya <evfi29@gmail.com>
2025-12-27 16:37:20 +0700

    merge

--- database/factories/HistoryStokFactory.php
+++ database/factories/HistoryStokFactory.php
@@ -2,11 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\historyStok;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class HistoryStokFactory extends Factory
 {
-     public function definition(): array
+    protected $model = historyStok::class;
+
+    public function definition(): array
     {
         return [
             'idProduk' => \App\Models\Produk::factory(),
